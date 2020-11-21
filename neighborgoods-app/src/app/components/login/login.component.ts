b5ba001a23def1acfb3675bc4028ed3af719852e
Marta Greguła <mgregula10@gmail.com>
2020-11-21 04:42:04 +0100

    Add login page

---
+++ neighborgoods-app/src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  
+  constructor() { }
+
+  // TODO - NIE DZIAŁA
+  loginForm = new FormGroup ({
+    emailFormControl: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    passwordFormControl: new FormControl('', [
+      Validators.required,
+    ])
+  })
+  
+  ngOnInit(): void {
+  }
+
+  public onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.loginForm.value);
+  }
+}
